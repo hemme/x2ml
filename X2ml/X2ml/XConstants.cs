e12e5779a693ac3622537dbb5f094d4554defe1a
hemme <hemme.dev@gmail.com>
2010-11-05 00:02:03 +0000

    Added some properties to aid in writing html with intellisense support

---
+++ X2ml/X2ml/XConstants.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace X2ml
+{
+    public  partial class X
+    {
+        private static readonly string _HTML = "html";
+        private static readonly string _BODY = "body";
+        private static readonly string _HEAD = "head";
+        private static readonly string _SPAN = "span";
+        private static readonly string _DIV = "div";
+        private static readonly string _LI = "li";
+        private static readonly string _UL = "ul";
+        private static readonly string _OL = "ol";
+        private static readonly string _TABLE = "table";
+        private static readonly string _TR = "tr";
+        private static readonly string _TH = "th";
+        private static readonly string _TBODY = "tbody";
+        private static readonly string _H1 = "h1";
+        private static readonly string _H2 = "h2";
+        private static readonly string _H3 = "h3";
+        private static readonly string _P = "p";
+        private static readonly string _LABEL = "label";
+        private static readonly string _INPUT = "input";
+        private static readonly string _FORM = "form";
+        private static readonly string _TEXTAREA = "textarea";
+        private static readonly string _IMG = "img";
+        private static readonly string _A = "a";
+
+        public static string HTML { get { return _HTML; } }
+        public static string BODY { get { return _BODY; } }
+        public static string HEAD { get { return _HEAD; } }
+        public static string SPAN { get { return _SPAN; } }
+        public static string DIV { get { return _DIV; } }
+        public static string LI { get { return _LI; } }
+        public static string UL { get { return _UL; } }
+        public static string OL { get { return _OL; } }
+        public static string TABLE { get { return _TABLE; } }
+        public static string TR { get { return _TR; } }
+        public static string TH { get { return _TH; } }
+        public static string TBODY { get { return _TBODY; } }
+        public static string H1 { get { return _H1; } }
+        public static string H2 { get { return _H2; } }
+        public static string H3 { get { return _H3; } }
+        public static string P { get { return _P; } }
+        public static string LABEL { get { return _LABEL; } }
+        public static string INPUT { get { return _INPUT; } }
+        public static string FORM { get { return _FORM; } }
+        public static string TEXTAREA { get { return _TEXTAREA; } }
+        public static string IMG { get { return _IMG; } }
+        public static string A { get { return _A; } }
+    }
+}
